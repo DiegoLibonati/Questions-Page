3d99c462075710e7f203ae592b8a6f6077247c64
Die <dlibonati@leafnoise.io>
2025-01-31 12:04:22 -0300

    fix: better bem css

--- src/helpers/getElements.ts
+++ src/helpers/getElements.ts
@@ -1,6 +1,6 @@
 export const getElements = () => ({
   btnsOpenQuestions: document.querySelectorAll(
-    ".question__header-btn "
+    ".question__btn-manage "
   ) as NodeList,
   questionOpen: document.querySelector(
     ".question.question--show"
